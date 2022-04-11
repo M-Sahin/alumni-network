2b34e5a9a6c3c77d1017789a20562a57f1ac366f
M-sahin <murat.sa25@gmail.com>
2022-04-11 10:40:46 +0200

    feat: added browserrouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Card from "react-bootstrap/Card";
 import React from "react";
-import LoginForm from "./components/login/LoginForm";
-import Navbar from "./components/navbar/Navbar";
+import Login from "./components/login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 
 
 function App() {
-  return (
-    <div className="App">
-         <Navbar />
-        <Card body>
-          <LoginForm />
-        </Card>
-      </div>
-    
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<Routes>
+					<Route path="/" element={<Login />} />
+          </Routes>
+          </div>
+          </BrowserRouter>   
   );
 }
 
