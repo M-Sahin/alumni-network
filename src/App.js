cc824b9470442d4e41b89df2dcc23a0c5a0a71a8
M-sahin <murat.sa25@gmail.com>
2022-04-11 10:12:37 +0200

    base template

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
 import React from "react";
 import LoginForm from "./components/login/LoginForm";
-import Navbar from "./components/navbar/Navbar";
+
 
 
 
 function App() {
   return (
     <div className="App">
-         <Navbar />
+        
         <Card body>
           <LoginForm />
         </Card>
